728ee01f6ddfde3474f4fccd09f18e3ac240ae06
Christopher Braun <Christopher-Braun@outlook.com>
2026-03-09 14:19:33 +0100

    Drag and drop function added.

--- js/board.js
+++ js/board.js
@@ -3,37 +3,76 @@ let currentDraggedElement;
 let todos = [
   {
     id: 0,
-    title: "Putzen",
+    title: "toDo",
     category: "toDo",
   },
   {
     id: 1,
-    title: "Kochen",
+    title: "todo2",
     category: "toDo",
   },
   {
     id: 2,
-    title: "Einkaufen",
+    title: "inProgress",
     category: "inProgress",
+  },
+  {
+    id: 3,
+    title: "Await",
+    category: "await",
+  },
+  {
+    id: 4,
+    title: "Done",
+    category: "done",
   },
 ];
 
 function updateHTML() {
-  let open = todos.filter((t) => t["category"] == "toDo");
+  toDo();
+  inProgress();
+  awaitFeedback();
+  done();
+}
+
+function toDo() {
+  let toDo = todos.filter((t) => t["category"] == "toDo");
   document.getElementById("toDo").innerHTML = "";
 
-  for (let i = 0; i < open.length; i++) {
-    const element = open[i];
+  for (let i = 0; i < toDo.length; i++) {
+    const element = toDo[i];
     document.getElementById("toDo").innerHTML += generateToDoHTML(element);
   }
+}
 
-  let closed = todos.filter((t) => t["category"] == "inProgress");
+function inProgress() {
+  let progress = todos.filter((t) => t["category"] == "inProgress");
   document.getElementById("inProgress").innerHTML = "";
 
-  for (let i = 0; i < closed.length; i++) {
-    const element = closed[i];
+  for (let i = 0; i < progress.length; i++) {
+    const element = progress[i];
     document.getElementById("inProgress").innerHTML +=
       generateToDoHTML(element);
+  }
+}
+
+function awaitFeedback() {
+  let awaitFeedback = todos.filter((t) => t["category"] == "await");
+  document.getElementById("await").innerHTML = "";
+
+  for (let i = 0; i < awaitFeedback.length; i++) {
+    const element = awaitFeedback[i];
+    document.getElementById("await").innerHTML += generateToDoHTML(element);
+  }
+}
+
+function done() {
+  let done = todos.filter((t) => t["category"] == "done");
+  document.getElementById("done").innerHTML = "";
+
+  for (let i = 0; i < done.length; i++) {
+    const element = done[i];
+    document.getElementById("done").innerHTML += generateToDoHTML(element);
   }
 }
 
@@ -41,6 +80,7 @@ function startDragging(id) {
   currentDraggedElement = id;
 }
 
+// This is the template 
 function generateToDoHTML(element) {
   return `<div draggable="true" ondragstart="startDragging(${element["id"]})" class="todo">${element["title"]}</div>`;
 }
