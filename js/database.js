88c55dfbd5361a14ed8410503141611323c18866
Christopher Braun <Christopher-Braun@outlook.com>
2026-03-11 21:23:54 +0100

    add Databank URL and tests with function loadDatabank()

--- js/database.js
+++ js/database.js
@@ -1,10 +1,19 @@
 const BASE_URL =
   "https://join-database-3e254-default-rtdb.europe-west1.firebasedatabase.app/";
 
+const CONCTACT_URL =
+  "https://join-database-3e254-default-rtdb.europe-west1.firebasedatabase.app/contacts/";
+
+  const TASK_URL =  "https://join-database-3e254-default-rtdb.europe-west1.firebasedatabase.app/tasks";
+
 function loadDatabank() {
   console.log("test");
   // loadData();
-  postData("/name", { BANANA: "RAMA" });
+  postData("/tasks", { Test: "ABC123",
+    Name: "Max",
+    Lastname: "Mustermann"
+
+   });
   // deleteData("/-OnOBfp3TMgEPEXlwYyC");
   // putData(path = "", data = {}) 
 }
