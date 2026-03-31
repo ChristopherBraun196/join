bac11a0597a308c30bd0577c45e73076b277c593
M. Siegrist <therealphantom22@proton.me>
2026-03-31 10:24:05 +0200

    responsive fixes and added time specific welcome msgs on summary

--- js/summaryAuth.js
+++ js/summaryAuth.js
@@ -2,20 +2,49 @@ import { auth, db } from "./firebaseAuth.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 import { ref, get } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-database.js";
 
+const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });   
+const greetingElement = document.getElementById('greeting-msg');
+const nameElement = document.querySelector(".welcome_name");
+
 onAuthStateChanged(auth, async (user) => {
   if (!user) {
     window.location.href = "./index.html";
     return;
   }
 
-  const nameElement = document.querySelector(".welcome_name");
-
   if (user.isAnonymous) {
-    nameElement.textContent = "Guest";
+    setGreeting("Guest");
   } else {
     const userSnap = await get(ref(db, `users/${user.uid}`));
     const contactId = userSnap.val()?.contactId;
     const contactSnap = await get(ref(db, `contacts/${contactId}`));
-    nameElement.textContent = contactSnap.val()?.name || user.email;
+    setGreeting(contactSnap.val()?.name || user.email);
   }
 });
+
+function getGreeting(timeString) {
+  const hour = parseInt(timeString.split(':')[0], 10);
+  let greetingPart;
+
+  switch (true) {
+    case (hour >= 5 && hour < 12):
+      greetingPart = "Good morning,";
+      break;
+    case (hour >= 12 && hour < 18):
+      greetingPart = "Good afternoon,";
+      break;
+    case (hour >= 18 || hour < 5):
+      greetingPart = "Good evening,";
+      break;
+    default:
+      greetingPart = "Hello,";
+      break;
+  }
+  return greetingPart;
+}
+
+function setGreeting(name) {
+  const greeting = getGreeting(currentTime);
+  greetingElement.textContent = greeting;
+  nameElement.textContent = name;
+}
