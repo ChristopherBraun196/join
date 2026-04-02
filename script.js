774e1c98e0f58b99e5ea98dc81b607774c924e86
Filip Rozanowski <filip.rozanowski@gmail.com>
2026-04-02 20:35:09 +0200

    console error fix

--- script.js
+++ script.js
@@ -135,6 +135,8 @@ function getInitials(name) {
 function renderUserMenue() {
     const user = window.currentUser;
     const um = document.getElementById("user-menue");
+    if (!um) return;
+    
     if (!user) um.innerText = 'G';
     else {
         const initials = getInitials(user.name);
@@ -179,4 +181,7 @@ function generateUUID() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
+
+    
 }
+
